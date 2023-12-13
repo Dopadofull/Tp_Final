c609a4d9aa51e4bf724ad410ad576e5721ee04b1
Dopadofull <melquistar@gmail.com>
2023-12-13 12:12:10 -0300

    avance con la actividad 8

--- Pelis/db.py
+++ Pelis/db.py
@@ -1,8 +1,20 @@
 import sqlite3
+import psycopg2
 
 import click
 from flask import current_app, g
 
+
+def get_db():
+    if 'db' not in g:
+        conn = psycopg2.connect(host="127.0.0.1",
+            database="Sakila",
+            user="postgres",
+            password="postgres",
+            port=5432)
+        g.db = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        
+    return g.db
 
 def get_db():
     if 'db' not in g:
@@ -37,3 +49,8 @@ def init_db_command():
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
+def dict_factory(cursor, row):
+    """Arma un diccionario con los valores de la fila."""
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
